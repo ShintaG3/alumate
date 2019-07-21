2704a602fd7d641a1b44f4c224353b943655b9a3
ShintaG3 <snishino2013@gmail.com>
2019-07-21 16:17:14 +0900

    university form change base.html change

--- my_universities/forms.py
+++ my_universities/forms.py
@@ -10,15 +10,71 @@ def current_year():
     return datetime.date.today().year
 
 def year_choices():
-    return [(r,r) for r in range(1970, datetime.date.today().year+5)]
+    return [(r,r) for r in range(1970, datetime.date.today().year+11)]
 
 class MyUniversityForm(forms.ModelForm):
-    st_year = forms.TypedChoiceField(label='Start Year', coerce=int, choices=year_choices, initial=current_year)
-    en_year = forms.TypedChoiceField(label='End Year', coerce=int, choices=year_choices, initial=current_year)
-    enr_status = forms.CharField(label='Enrollment Status', disabled=True)
+    st_year         = forms.TypedChoiceField(label='Start Year', coerce=int, choices=year_choices,
+                        initial=current_year, widget=forms.Select(
+                                attrs={
+                                    # 'placeholder': '',
+                                    'name': 'en_year',
+                                    'id': 'en_year',
+                                    'class':'form-control',
+                                }
+                            )
+
+                        )
+
+    en_year         = forms.TypedChoiceField(label='End Year', coerce=int, choices=year_choices,
+                        initial=current_year, widget=forms.Select(
+                                attrs={
+                                    # 'placeholder': '',
+                                    'name': 'en_year',
+                                    'id': 'en_year',
+                                    'class':'form-control',
+                                }
+                            )
+
+                        )
+    uni_name        = forms.CharField(label='School', widget=forms.TextInput(
+                                    attrs={
+                                        'placeholder': 'Ex: Boston University',
+                                        'name': 'uni_name',
+                                        'id': 'uni_name',
+                                        'class':'form-control'
+                                    }
+                                )
+                            )
+    degree          = forms.CharField(label='Degree', widget=forms.TextInput(
+                                    attrs={
+                                        'placeholder': "Ex: Master's",
+                                        'class':'form-control',
+                                        'name':'degree',
+                                        'id': 'degree',
+                                    }
+                                )
+                            )
+    field_of_study  = forms.CharField(label='Field of stud', widget=forms.TextInput(
+                                    attrs={
+                                        'placeholder': 'Business',
+                                        'class':'form-control',
+                                        'name':'field_of_study',
+                                        'id': 'field_of_study',
+                                    }
+                                )
+                            )
+    grade  = forms.CharField(label='Grade', widget=forms.TextInput(
+                                    attrs={
+                                        'placeholder': 'Disticntion',
+                                        'class':'form-control',
+                                        'name':'grade',
+                                        'id': 'grade',
+                                    }
+                                )
+                            )
     class Meta:
         model = MyUniversity
-        fields =('country', 'uni_name','st_year','en_year','enr_status')
+        fields =('uni_name', 'degree', 'field_of_study', 'st_year','en_year','grade')
         labels = {
             'uni_name':_('University'),
             'enr_status':_('Enrollment Status'),
--- my_universities/models.py
+++ my_universities/models.py
@@ -11,7 +11,7 @@ def current_year():
     return datetime.date.today().year
 
 def max_value_current_year(value):
-    return MaxValueValidator(current_year() + 5)(value)
+    return MaxValueValidator(current_year() + 11)(value)
 
 ENROLLMENT_STATUS = (
     ('applicant','Applicant'),
@@ -20,15 +20,23 @@ ENROLLMENT_STATUS = (
 )
 
 class MyUniversity(models.Model):
-    user        = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    country     = models.CharField(max_length=256, null=True, blank=True)
-    uni_name    = models.CharField(max_length=100, null=True, blank=True)
-    st_year     = models.IntegerField(_('year'), validators=[MinValueValidator(1970), max_value_current_year],default=current_year)
-    en_year     = models.IntegerField(_('en_year'), validators=[MinValueValidator(1970), max_value_current_year],default=current_year)
-    enr_status  = models.CharField(max_length=64, choices=ENROLLMENT_STATUS)
+    user                = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    country             = models.CharField(max_length=256, null=True, blank=True)
+    uni_name            = models.CharField(max_length=256, null=True, blank=True)
+    degree              = models.CharField(max_length=256, null=True, blank=True)
+    field_of_study      = models.CharField(max_length=256, null=True, blank=True)
+    st_year             = models.IntegerField(_('year'), null=True, blank=True, validators=[MinValueValidator(1970), max_value_current_year],default=current_year)
+    en_year             = models.IntegerField(_('en_year'), null=True, blank=True, validators=[MinValueValidator(1970), max_value_current_year],default=current_year)
+    grade               = models.CharField(max_length=256, null=True, blank=True)
+    grade_rate          = models.IntegerField(default=60, null=True, blank=True)
+    # foundation_body     = models.CharField(max_length=256, null=True, blank=True)
+    # foundation_payback  = models.BooleanField(default=True)
+    # foundation_amount   = models.CharField(max_length=256, null=True, blank=True)
+    description         = models.CharField(max_length=1000, null=True, blank=True)
+    enr_status          = models.CharField(max_length=64, choices=ENROLLMENT_STATUS)
 
     def __str__(self):
-        return f"{self.id} {str(self.user)}"
+        return f"{self.id} {str(self.user)} {self.school}"
 
 def pre_save_userprofile_receiver(sender, instance, *args, **kwargs):
     en_year = instance.en_year
